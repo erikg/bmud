45d643d9871f934c0cff4c2014645d256f922992
erik <erik>
2004-01-18 15:45:40 +0000

    removed stars

--- src/authors.h
+++ src/authors.h
@@ -1,7 +1,7 @@
 const char *authors[] = {
-    "Erik Greenwald <br0ke@math.smsu.edu> <erik@smluc.org>\n\t* Main author/maintainer.",
-    "Erik Thyren\n\t* GTK+ connect wizard",
-    "Robert J. Hansen <rjhansen@inav.net>\n\t* rewrite of net.c",
-    "Dan Tenner\n\t* aliases",
+    "Erik Greenwald <br0ke@math.smsu.edu> <erik@smluc.org>\n\tMain author/maintainer.",
+    "Erik Thyren\n\tGTK+ connect wizard",
+    "Robert J. Hansen <rjhansen@inav.net>\n\trewrite of net.c",
+    "Dan Tenner\n\taliases",
     NULL
 };
